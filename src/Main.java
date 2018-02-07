d5b06e7785eda4bf30563d94b932f21751be46dd
Zhuo Chen <zhuoc3@illinois.edu>
2018-02-06 21:38:22 -0600

    1111

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,11 @@ public class Main {
         String punctuation = "~ ` ! @ # $ % ^ & * ( ) _ + - = { } [ ] | \\ : ; \" ' < > , . ? /";
         String[] punctuationList = punctuation.split(" ");
         String[] numberList = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "0"};
-        }
+        String[] c1 = challen1.split(" ");
+        String[] c2 = challen2.split(" ");
+        String[] c3 = challen3.split(" ");
+        System.out.println("Website 1 has " + c1.length + " words");
+        System.out.println("Website 2 has " + c2.length + " words");
+        System.out.println("Website 3 has " + c3.length + " words");
+    }
 }
